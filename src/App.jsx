6adaaecc7e4acbba7861d75951cf267986190f76
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-06-29 10:39:35 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Components/Header/Header'
+import Banner from './Components/Banner/Banner'
+import '@fontsource/roboto'
+import '@fontsource/source-sans-pro'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  )
+}
+
+export default App
