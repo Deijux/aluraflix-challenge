eb6afe8f87de9406a210d4f32c617f5571873df3
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-07-06 12:09:59 -0500

    Se importó la etiqueta NavLink de React Router, y se añadieron nuevos estilos

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import logoHeader from '../../assets/logoHeader.png'
 import addDefault from '../../assets/add-default.png'
 import homeDefault from '../../assets/home-default.png'
@@ -6,19 +7,31 @@ import Styles from './Header.module.css'
 function Header() {
   return (
     <header className={Styles.header}>
-      <img src={logoHeader} alt='Aluraflix Logo' />
+      <NavLink to='/'>
+        <img src={logoHeader} alt='Aluraflix Logo' />
+      </NavLink>
       <ul>
         <li>
-          <a href=''>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? `${Styles.link} ${Styles.linkActive}` : Styles.link
+            }
+          >
             <img src={homeDefault} alt='Home Icon' />
-            <p>Home</p>
-          </a>
+            <p>HOME</p>
+          </NavLink>
         </li>
         <li>
-          <a href=''>
+          <NavLink
+            to='/newvideo'
+            className={({ isActive }) =>
+              isActive ? `${Styles.link} ${Styles.linkActive}` : Styles.link
+            }
+          >
             <img src={addDefault} alt='Añadir Icon' />
-            <p>Nuevo Video</p>
-          </a>
+            <p>NUEVO VIDEO</p>
+          </NavLink>
         </li>
       </ul>
     </header>
