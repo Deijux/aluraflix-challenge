91b67c30b197a8ee8242b7222194c243d2e8ffda
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-07-06 12:14:50 -0500

    Se añade validación para el componente OptionList

--- src/Components/OptionList/OptionList.jsx
+++ src/Components/OptionList/OptionList.jsx
@@ -1,9 +1,19 @@
 import { PropTypes } from 'prop-types'
 import Style from './OptionList.module.css'
 
-function OptionList({ name, id, optionsAll }) {
+function OptionList({ section, name, id, optionsAll }) {
+  let styles = {}
+
+  if (section == 'addVideo') {
+    styles = {
+      border: '3px solid #262626',
+      borderRadius: '4px',
+      padding: '10px 5px',
+    }
+  }
+
   return (
-    <select className={Style.select} name={name} id={id}>
+    <select style={styles} className={Style.select} name={name} id={id}>
       {optionsAll.map((options, index) => (
         <option className={Style.option} key={index} value={options.name}>
           {options.name}
@@ -14,6 +24,7 @@ function OptionList({ name, id, optionsAll }) {
 }
 
 OptionList.propTypes = {
+  section: PropTypes.string,
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   optionsAll: PropTypes.array.isRequired,
