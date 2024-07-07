9e8f2b428ab0cff89b6516c101652a4fe2cccab8
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-07-07 17:56:12 -0500

    Se importó el DataProvider

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from '../Components/Header/Header.jsx'
+import Header from '../components/Header/Header.jsx'
 import Home from '../pages/Home/Home.jsx'
 import NewVideo from '../pages/NewVideo/NewVideo.jsx'
-import Footer from '../Components/Footer/Footer.jsx'
+import Footer from '../components/Footer/Footer.jsx'
+import DataProvider from '../contexts/DataContext.jsx'
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route index element={<Home />}></Route>
-        <Route path='newvideo' element={<NewVideo />}></Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <DataProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path='newvideo' element={<NewVideo />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </DataProvider>
   )
 }
 
