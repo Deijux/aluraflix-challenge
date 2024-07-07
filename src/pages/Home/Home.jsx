339d5f3e755ed460f0acda4effb5b6baa2a5c6ef
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-07-07 17:58:30 -0500

    Se añaden diferentes cambios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from 'react'
-import Banner from '../../Components/Banner/Banner'
-import Category from '../../Components/Category/Category'
-import Modal from '../../Components/Modal/Modal'
-import categoryData from '../../data/CategoryData'
+import { useState } from 'react'
+import Banner from '../../components/Banner/Banner'
+import Category from '../../components/Category/Category'
+import Modal from '../../components/Modal/Modal'
+import { useData } from '../../contexts/DataContext'
 import Style from './Home.module.css'
 
 function Home() {
-  const [categories, setCategories] = useState([])
+  const { videos, categories, banner } = useData()
   const [modalOpen, setModalOpen] = useState(false)
+
   const handleModalClose = () => {
     setModalOpen(false)
   }
@@ -16,18 +17,15 @@ function Home() {
     setModalOpen(true)
   }
 
-  useEffect(() => {
-    setCategories(categoryData)
-  }, [])
-
   return (
     <>
       <main className={Style.main}>
-        <Banner />
+        <Banner data={banner} />
         {categories.map(category => (
           <Category
             key={category.id}
             datos={category}
+            cards={videos.filter(video => video.category === category.name)}
             onCardEdit={handleCardEdit}
           />
         ))}
