76df253ab82a5fe20646e02b7c8cf3fc08d5c379
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-07-08 07:28:55 -0500

    Se añade condiciones para la renderización

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -11,21 +11,35 @@ function Banner({ data }) {
         src={backgroundBanner}
         alt='Imagen background'
       />
-      <div className={Style.titleBanner}>
-        <h1>{dataInfo?.category}</h1>
-        <div className={Style.infoBanner}>
-          <h2>{dataInfo?.title}</h2>
-          <p>{dataInfo?.description}</p>
+      {dataInfo && (
+        <>
+          <div className={Style.titleBanner}>
+            <h1>{dataInfo?.category}</h1>
+            <div className={Style.infoBanner}>
+              <h2>{dataInfo?.title}</h2>
+              <p>{dataInfo?.description}</p>
+            </div>
+          </div>
+          <a
+            className={Style.imageLink}
+            href={dataInfo?.link}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className={Style.playerImage}
+              src={dataInfo?.photo}
+              alt='Player'
+            />
+          </a>
+        </>
+      )}
+
+      {dataInfo === undefined && (
+        <div className={Style.titleBanner}>
+          <h2>Ocurrió un error con el servidor!</h2>
         </div>
-      </div>
-      <a
-        className={Style.imageLink}
-        href={dataInfo?.link}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <img className={Style.playerImage} src={dataInfo?.photo} alt='Player' />
-      </a>
+      )}
     </section>
   )
 }
