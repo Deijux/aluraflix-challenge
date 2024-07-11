6a4fda9b572bce17f0c8838aca002278e2fa220a
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-07-11 17:56:44 -0500

    Se añade imagen optimizada y se importa al componente Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import { PropTypes } from 'prop-types'
-import backgroundBanner from '../../assets/backgroundBanner.png'
+import backgroundBanner from '../../assets/backgroundBanner.webp'
 import Style from './Banner.module.css'
 
 function Banner({ data }) {
